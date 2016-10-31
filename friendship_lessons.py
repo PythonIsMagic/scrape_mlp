da77f7c1917e57eda9ec9fa7e12c48d745ba7d8b
Erik Lunna <eslunna@gmail.com>
2016-10-30 21:20:54 -0400

    Updated the command line options for friendship_lessons, uses argparse now.

--- friendship_lessons.py
+++ friendship_lessons.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
+import argparse
 import scrapekit
-import sys
 import textwrap
 """
     Date: 10/15/16
@@ -16,45 +16,46 @@ import textwrap
 URL = 'http://mlp.wikia.com/wiki/Friendship_lessons'
 
 if __name__ == "__main__":
-    download = False
-    usage = """
-Usage: {} [-d]'.format(sys.argv[0])
-    -d downloads all friendship lessons to text file.
-    No arguments - display all friendship lessons.
-    """
-
-    if len(sys.argv) == 2:
-        if sys.argv[1] == '-d':
-            download = True
-        elif sys.argv[1] in ['?', '--h', '-h']:
-            print(usage)
-            exit()
-    elif len(sys.argv) > 2:
-        print(usage)
-        exit()
+    parser = argparse.ArgumentParser(description="Get all friendship lessons from the show.")
+    parser.add_argument('-v', '--verbose', action="store_true",
+                        help="Display more info about processing.")
+    parser.add_argument('-d', '--download', action='store_true',
+                        help="Download all friendship lessons to text file.")
+    parser.add_argument('-o', '--format', action='store_true',
+                        help='Formats the text for better readability.')
+    args = parser.parse_args()
 
     soup = scrapekit.handle_url(URL)
     spans = soup.findAll('span', {'class': 'mw-headline'})
 
     lessons = []
 
+    # Collect lessons text
     for span in spans:
         h3 = span.parent
         dl = h3.findNext('dl')
         if dl:
-            lines = dl.text.encode('utf-8').split('\n')
-            text = ''
-            for l in lines:
-                for l in textwrap.wrap(l, 78):
-                    text += (l + '\n')
-            print(text)
+            text = dl.text.encode('utf-8')
             lessons.append(text)
 
-    if download:
+    if args.format:
+        print('Formatting text...')
+
+        for x, lesson in enumerate(lessons[:]):
+            text = ''
+            lines = lesson.split('\n')
+            for i in lines:
+                for i in textwrap.wrap(i, 78):
+                    text += i + '\n'
+            lessons[x] = text
+
+    if args.verbose:
+        for i in lessons:
+            print(i)
+
+    if args.download:
         filename = scrapekit.DATADIR + 'lessons.txt'
 
         with open(filename, 'w') as f:
-            sep = '\n' + '~-'*40 + '\n\n'
-            for l in lessons:
-                f.write(l)
-                f.write(sep)
+            for i in lessons:
+                f.write(i)
